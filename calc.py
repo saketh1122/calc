598d07e1b5c37c5656b694cff0e01735374763dd
saketh1122 <112796631+saketh1122@users.noreply.github.com>
2025-09-28 21:33:39 +0530

    initial commit

---
+++ calc.py
@@ -0,0 +1,20 @@
+import math
+
+def sqrt_func(x):
+    return math.sqrt(x)
+
+def fact_func(x):
+    return math.factorial(x)
+
+def ln_func(x):
+    return math.log(x)
+
+def power_func(x, b):
+    return math.pow(x, b)
+
+if __name__ == "__main__":
+    print("Scientific Calculator")
+    print("sqrt(16) =", sqrt_func(16))
+    print("factorial(5) =", fact_func(5))
+    print("ln(2.71828) =", ln_func(2.71828))
+    print("2^3 =", power_func(2, 3))
